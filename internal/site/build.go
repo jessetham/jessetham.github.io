03e30b06405ea70b2ded8bea3eddb2efa3836ac0
Jesse Tham <9289224+jessetham@users.noreply.github.com>
2026-05-22 10:16:27 -0700

    feat(site): Config struct and copyStatic helper

---
+++ internal/site/build.go
@@ -0,0 +1,56 @@
+package site
+
+import (
+	"errors"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	ContentDir   string
+	TemplatesDir string
+	StaticDir    string
+	OutDir       string
+}
+
+func copyStatic(src, dst string) error {
+	if _, err := os.Stat(src); errors.Is(err, fs.ErrNotExist) {
+		return nil
+	} else if err != nil {
+		return err
+	}
+	return filepath.WalkDir(src, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		rel, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+		target := filepath.Join(dst, rel)
+		if d.IsDir() {
+			return os.MkdirAll(target, 0755)
+		}
+		return copyFile(path, target)
+	})
+}
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	if err := os.MkdirAll(filepath.Dir(dst), 0755); err != nil {
+		return err
+	}
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, in)
+	return err
+}
